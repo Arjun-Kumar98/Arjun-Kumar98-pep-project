633118177fba4f2be190a5ae61d0d486a3e7b531
Arjun Kumar <mrarjunkumar42@gmail.com>
2025-01-25 16:55:39 +0000

    Dao base template done

--- src/main/java/DAO/SocialMediaDAO.java
+++ src/main/java/DAO/SocialMediaDAO.java
@@ -1,13 +1,41 @@
 package DAO;
 import Model.Account;
 import Model.Message;
+import java.util.List;
 public class SocialMediaDAO {
 
-    public Account registerUser(Account account){
-       
-        return null;
+public Account registerUser(Account account){
+    return null;
 }
 public int findUser(String userName){
     return 0;
+}
+
+public Account loginUser(Account account){
+
+return null;
+}
+
+public Message createMessage(Message message){
+
+return null;
+}
+
+public List<Message> retrieveMessage(){
+
+return null;
+}
+
+public void deleteMessage(Integer messageId){
+
+}
+
+public Message updateMessage(Message message){
+return null;
+}
+
+public List<Message> getMessages(Integer userId){
+
+return null;
 }
 }
