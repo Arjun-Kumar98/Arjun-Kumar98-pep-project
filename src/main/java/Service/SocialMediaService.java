27d3b040b4ae97ad6826e96daa3d54a714ecaeb6
Arjun Kumar <mrarjunkumar42@gmail.com>
2025-01-25 17:27:30 +0000

    service first function

--- src/main/java/Service/SocialMediaService.java
+++ src/main/java/Service/SocialMediaService.java
@@ -1,5 +1,28 @@
 package Service;
-
+import Model.Message;
+import Model.Account;
+import DAO.SocialMediaDAO;
 public class SocialMediaService {
     
+SocialMediaDAO socialmediaDAO;
+
+public SocialMediaService(){
+   socialmediaDAO = new SocialMediaDAO();
+} 
+
+public SocialMediaService(SocialMediaDAO socialmediaDAO){
+   this.socialmediaDAO = socialmediaDAO;
+}
+
+public Account addAccount(Account account){
+String username = account.getUsername();
+String password = account.getPassword();
+if(username.length()>=1&&password.length()>=4){
+int count = socialmediaDAO.findUser(username);
+if(count==0){
+return socialmediaDAO.registerUser(account);
+}
+}
+return null;
+}
 }
